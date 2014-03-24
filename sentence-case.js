1833193194c24615a7befa517853629ebad5d28e
Blake Embrey <hello@blakeembrey.com>
2014-03-24 19:23:34 -0300

    Initial commit.

---
+++ sentence-case.js
@@ -0,0 +1,14 @@
+/**
+ * Sentence case a string.
+ *
+ * @param  {String} string
+ * @return {String}
+ */
+module.exports = function (string) {
+  return string
+    .toString()
+    .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+    .replace(/[^a-zA-Z0-9]+/g, ' ')
+    .replace(/^ | $/, '')
+    .toLowerCase();
+};
---
+++ test.js
@@ -0,0 +1,19 @@
+/* global describe, it */
+var assert       = require('assert');
+var sentenceCase = require('./');
+
+describe('sentence case', function () {
+  it('should lower case a single word', function () {
+    assert.equal(sentenceCase('test'), 'test');
+    assert.equal(sentenceCase('TEST'), 'test');
+  });
+
+  it('should sentence case camel cased strings', function () {
+    assert.equal(sentenceCase('testString'), 'test string');
+  });
+
+  it('should sentence case non-alphanumeric separators', function () {
+    assert.equal(sentenceCase('dot.case'), 'dot case');
+    assert.equal(sentenceCase('path/case'), 'path case');
+  });
+});
